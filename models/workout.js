ad966582fdd496ed0e33763dfef7c63391b33282
Josh Watkins <joshwatkinssfs@gmail.com>
2021-11-10 22:51:11 -0500

    workout model constructed

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
