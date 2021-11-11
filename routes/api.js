3ef190fd967eaaab5340f5c76cef9507eb97aa9e
Josh Watkins <joshwatkinssfs@gmail.com>
2021-11-11 02:14:19 -0500

    fixed routes

--- routes/api.js
+++ routes/api.js
@@ -3,22 +3,49 @@ const db = require("../models/workout.js");
 const path = require("path");
 
 router.get("/api/workouts/", (req, res) =>
-  db.Workout.find({})
+  //   db.Workout.find({})
+  db.Workout.aggregate([
+    // { match: { _id: req.params.id } },
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalDistance: { $sum: "$exercises.distance" },
+        totalSets: { $sum: "$exercises.sets" },
+        totalReps: { $sum: "$exercises.reps" },
+        totalWeight: { $sum: "$exercises.weight" },
+      },
+    },
+  ])
     .then((workout) => res.json(workout))
     .catch((err) => res.status(400).json(err))
 );
 router.get("/api/workouts/range/", ({}, res) =>
-  db.Workout.find({})
+  //   db.Workout.find({})
+  db.Workout.aggregate([
+    // { match: { _id: req.params.id } },
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalDistance: { $sum: "$exercises.distance" },
+        totalSets: { $sum: "$exercises.sets" },
+        totalReps: { $sum: "$exercises.reps" },
+        totalWeight: { $sum: "$exercises.weight" },
+      },
+    },
+  ])
     .then((workout) => res.json(workout))
     .catch((err) => res.status(400).json(err))
 );
 router.post("/api/workouts/", (req, res) =>
-  db.Workout.create(req.body)
+  db.Workout.create({})
     .then((workout) => res.json(workout))
     .catch((err) => res.status(400).json(err))
 );
 router.put("/api/workouts/:id", (req, res) =>
-  db.Workout.findByIdAndUpdate({ _id: req.params.id }, { exercises: req.body })
+  db.Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
     .then((workout) => res.json(workout))
     .catch((err) => res.status(400).json(err))
 );
